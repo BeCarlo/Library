08bb6e847932cc6801cce127677719f523440635
carlo berlingeri <berlingeri.carlo@gmail.com>
2025-08-26 08:34:05 +0000

    Upload Files

---
+++ MathVariable.h
@@ -0,0 +1,13 @@
+#ifndef MATH_VARIABLE_H
+#define MATH_VARIABLE_H
+
+#include <math.h>
+
+typedef float Scalar;
+
+static const double epsilon = 1.17549435E-38;
+static const double pi = 3.141592653589793;
+
+
+#endif // !MATH_VARIABLE_H
+
